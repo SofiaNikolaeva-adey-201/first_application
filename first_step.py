0a5e21ceba0104fda3b601284d004c0d943ffd3d
София Николаева <NikolaevaSG040@mgpu.ru>
2021-01-24 15:29:27 +0300

    2

--- first_step.py
+++ first_step.py
@@ -14,12 +14,38 @@ def on_click():
     print(form.plainTextEdit.toPlainText())
     print(form.dateEdit.dateTime().toString('dd-MM-yyyy'))
     print("Clicked!")
-    print(form.calendarWidget.selectedDate().toString('dd-MM-yyyy'))
-    date = QDate(2022, 9, 17)
-    form.calendarWidget.setSelectedDate(date)
+    #print(form.calendarWidget.selectedDate().toString('dd-MM-yyyy'))
+    #date = QDate(2022, 9, 17)
+    #form.calendarWidget.setSelectedDate(date)
+
+def on_click_calendar():
+    global start_date, calc_date
+    #print(form.calendarWidget.selectedDate().toString('dd-MM-yyyy'))
+    form.dateEdit.setDate(form.calendarWidget.selectedDate())
+    calc_date = form.calendarWidget.selectedDate()
+    delta_days = start_date.daysTo(calc_date)
+    print(delta_days)
+
+
+def on_dateedit_change():
+    global start_date, calc_date
+    #print(form.dateEdit.dateTime().toString('dd-MM-yyyy'))
+    form.calendarWidget.setSelectedDate(form.dateEdit.date())
+    calc_date = form.calendarWidget.selectedDate()
+    delta_days = start_date.daysTo(calc_date)
+    print(delta_days)
+
+
 
 
 form.pushButton.clicked.connect(on_click)
+form.calendarWidget.clicked.connect(on_click_calendar)
+form.dateEdit.dateChanged.connect(on_dateedit_change)
+
+start_date = form.calendarWidget.selectedDate()
+calc_date = form.calendarWidget.selectedDate()
+
+on_click_calendar()
 
 
 app.exec_()
